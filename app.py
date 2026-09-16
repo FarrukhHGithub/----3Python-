6de362c8461b7d63bbde9892716d9d681e27f3d0
Farrukh Hussain Balay <FarrukhHGithub>
2026-09-16 11:03:43 +0500

    creating a landing page on python

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def server_error(e):
     return render_template("errors/500.html", title="500 Internal Error"), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
